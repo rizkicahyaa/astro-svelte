489d5af08a5413e0d8de6130777ad5f263601de6
Rizki Cahya <rizki.cahya03@gmail.com>
2026-06-14 17:14:14 +0700

    feat: count test

---
+++ src/components/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+    let count = 0;
+</script>
+
+<p>Count: {count}</p>
+
+<button on:click={() => count++}>
+    Tambah
+</button>
+
+<button on:click={() => count--}>
+    Kurang
+</button>
+
+<style>
+    button {
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+</style>
